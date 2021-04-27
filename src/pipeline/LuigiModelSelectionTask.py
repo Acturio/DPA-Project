0c2b6252a4b8988469a90a4e25512b3218864613
Acturio <act.arturo.b@ciencias.unam.mx>
2021-04-27 05:58:14 +0000

    Se agregan tasks de metadataTraining y ModelSelection

---
+++ src/pipeline/LuigiModelSelectionTask.py
@@ -0,0 +1,76 @@
+import luigi
+import boto3
+import pandas as pd
+import pickle
+import luigi.contrib.s3
+import src.utils.constants as cte
+from src.utils import general as gral
+from src.pipeline.ingesta_almacenamiento import get_s3_client, guardar_ingesta
+from src.pipeline.LuigiTrainingMetadataTask import TrainingModelMetadataTask
+from src.pipeline import modeling as mod
+
+
+class ModelSelectionTask(luigi.Task):
+
+  path_cred = luigi.Parameter(default = 'credentials.yaml')
+  initial = luigi.BoolParameter(default=True, parsing = luigi.BoolParameter.EXPLICIT_PARSING)
+  limit = luigi.IntParameter(default = 300000)
+  date = luigi.DateParameter(default = None)
+  initial_date = luigi.DateParameter(default = None)
+  bucket_path = luigi.Parameter(default = cte.BUCKET)
+  exercise = luigi.BoolParameter(default=False, parsing = luigi.BoolParameter.EXPLICIT_PARSING)
+
+    # Se requiere TrainingMetadataTask
+  def requires(self):
+    return TrainingModelMetadataTask(
+      self.path_cred,
+      self.initial,
+      self.limit,
+      self.date,
+      self.initial_date,
+      self.bucket_path,
+      self.exercise
+    )
+
+# Se carga el archivo sobre el cual se realizará el feature engineering
+  def input(self):
+
+    file_name = "models/training-models/food-inspections-models-" + '{}.pkl'.format(self.date.strftime('%Y-%m-%d'))
+    s3 = get_s3_client(self.path_cred)
+    s3_object = s3.get_object(Bucket = self.bucket_path, Key = file_name)
+    body = s3_object['Body']
+    model = pickle.loads(body.read())
+    
+    return model
+
+
+  def output(self):
+
+    s3_c = gral.get_s3_credentials(self.path_cred)
+    client_s3 = luigi.contrib.s3.S3Client(
+      aws_access_key_id = s3_c["aws_access_key_id"],
+      aws_secret_access_key = s3_c["aws_secret_access_key"]
+      )
+    
+    file_type = "models/best-models/best-food-inspections-model-"
+    output_path = "s3://{}/{}{}.pkl".format(cte.BUCKET, file_type, self.date.strftime('%Y-%m-%d'))
+    
+    return luigi.contrib.s3.S3Target(path = output_path, client = client_s3)
+
+  
+  def run(self):
+
+    models = self.input()
+    file_type = "models/best-models/best-model-consecutive-"
+    output_path = "{}{}.pkl".format(file_type, self.date.strftime('%Y-%m-%d'))
+    local_path = "results/" + output_path + self.date.strftime('%Y-%m-%d') + ".pkl"
+    
+    best_model = mod.best_model(models, local_path)
+    
+    guardar_ingesta(
+      path_cred = self.path_cred,
+      bucket = self.bucket_path,
+      bucket_path = file_type,
+      data = best_model,
+      fecha = self.date.strftime('%Y-%m-%d')
+    )
---
+++ src/pipeline/LuigiTrainingMetadataTask.py
@@ -0,0 +1,84 @@
+from luigi.contrib.postgres import CopyToTable
+from src.utils.general import transform_metadata, read_yaml_file
+from src.utils.utils import load_df
+from src.pipeline.LuigiTrainingTestTask import TrainingModelTestTask
+from src.pipeline.ingesta_almacenamiento import get_s3_client
+from src.pipeline import modeling as mod
+import src.utils.constants as cte
+import pandas as pd
+import luigi
+import psycopg2
+import yaml
+import pickle
+
+
+class TrainingModelMetadataTask(CopyToTable):
+
+	path_cred = luigi.Parameter(default = 'credentials.yaml')
+	initial = luigi.BoolParameter(default=True, parsing = luigi.BoolParameter.EXPLICIT_PARSING)
+	limit = luigi.IntParameter(default = 300000)
+	date = luigi.DateParameter(default = None)
+	initial_date = luigi.DateParameter(default = None)
+	bucket_path = luigi.Parameter(default = cte.BUCKET)
+	exercise = luigi.BoolParameter(default=False, parsing = luigi.BoolParameter.EXPLICIT_PARSING)
+
+	with open(cte.CREDENTIALS, 'r') as f:
+		config = yaml.safe_load(f)
+
+	credentials = config['db']
+
+	user = credentials['user']
+	password = credentials['pass']
+	database = credentials['database']
+	host = credentials['host']
+	port = credentials['port']
+
+	table = 'metadata.entrenamiento'
+
+	columns = [("processing_date", "TIMESTAMPTZ"),
+             ("data_date", "DATE"),
+             ("estimator", "VARCHAR"),
+             ("scoring", "VARCHAR"),
+             ("params", "VARCHAR"),
+             ("mean_test_score", "DOUBLE"),
+             ("rank_test_score","INTEGER")
+             ]
+
+
+	def requires(self):
+		return TrainingModelTestTask(
+      					self.path_cred,
+      					self.initial,
+     					  self.limit,
+      					self.date,
+      					self.initial_date,
+      					self.bucket_path,
+								self.exercise
+      					)
+
+	def input(self):
+
+		file_name = "models/training-models/food-inspections-models-" + '{}.pkl'.format(self.date.strftime('%Y-%m-%d'))
+
+		s3 = get_s3_client(self.path_cred)
+		s3_object = s3.get_object(Bucket = self.bucket_path, Key = file_name)
+		body = s3_object['Body']
+		training_models = pickle.loads(body.read())
+		
+		return training_models
+
+	def rows(self):
+		
+		models_filename = "results/models/training-models/food-inspections-models-metadata-" 
+		models_filename = models_filename + self.date.strftime('%Y-%m-%d') + ".pkl"
+		 
+		data = mod.metadata_models(
+			self.input(), 
+			date = self.date.strftime('%Y-%m-%d'),
+			path_save_metadata = models_filename
+		)
+		records = data.to_records(index=False)
+		r = list(records)
+    
+		for element in r:
+			yield element
