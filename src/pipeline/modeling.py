ddb0af97b31d478197519d8446d16052de492592
Acturio <act.arturo.b@ciencias.unam.mx>
2021-04-27 10:08:24 +0000

    Se corrige funcion de metadatos para elegir mejor modelo

--- src/pipeline/modeling.py
+++ src/pipeline/modeling.py
@@ -210,15 +210,11 @@ def best_model(models_ejercicio):
 ### METADATA BEST MODEL  ###
 def metadata_best_model(best_model, data_date) : 
     
-    df = pd.DataFrame(columns=['processing_date','data_date', 'base_estimator','params', 'value_params', 'num_features', 'oob_score'])
+    df = pd.DataFrame(columns=['processing_date','data_date','value_params'])
     
-    df['processing_date'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    df['data_date']= [fecha]
-    df['base_estimator']= ([best_model.base_estimator])
-    df['params']=([best_model.estimator_params ])
+    df['processing_date'] = [strftime("%Y-%m-%d %H:%M:%S", gmtime())]
+    df['data_date']= [data_date]
     df['value_params'] = (best_model.get_params)
-    df['num_features'] = (best_model.n_features_)
-    df['oob_score'] = (best_model.oob_score_)
         
     return (df)
 
