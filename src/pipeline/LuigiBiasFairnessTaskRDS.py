7953cd559a10fc5143345605cc5f5f528b47fbd4
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2021-05-22 09:47:54 -0500

    Agregando parametro

--- src/pipeline/LuigiBiasFairnessTaskRDS.py
+++ src/pipeline/LuigiBiasFairnessTaskRDS.py
@@ -150,7 +150,8 @@ class BiasFairnessTask(CopyToTable):
       df_fe = data, 
       best_model = best_model,
       auto_variables = features,
-      fecha = self.date.strftime('%Y-%m-%d')
+      fecha = self.date.strftime('%Y-%m-%d'),
+      inicial = self.initial
     )
 
     records = data.to_records(index=False)
