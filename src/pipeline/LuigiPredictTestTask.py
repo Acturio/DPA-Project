aba11a1d278410ac76b12cfc7404a3ced05b75d5
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2021-05-25 19:24:12 -0500

    modif

--- src/pipeline/LuigiPredictTestTask.py
+++ src/pipeline/LuigiPredictTestTask.py
@@ -139,7 +139,7 @@ class PredictTestTask(CopyToTable):
     file_name = "predict-" + self.date.strftime('%Y-%m-%d')
     #file_name_model = "best-food-inspections-model-" + self.date_bestmodel.strftime('%Y-%m-%d')
     data_in = self.input()
-    test = PredictTest(data = data_in, my_date = self.date)
+    test = PredictTest(data = data_in, my_date = self.date_bestmodel)
     """
     test = PredictTest(path_cred = self.path_cred, data = data["data"],\
                        my_date = self.date, model=data["best_model"])"""
